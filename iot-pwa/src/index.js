a81e9ffec366cf3459bfb36ad3fcb3a3b19adb8e
Karri <karrit@proton.me>
2026-01-06 10:06:59 +0200

    Initial PWA template with migrated IoT dashboard

--- iot-pwa/src/index.js
+++ iot-pwa/src/index.js
@@ -6,15 +6,18 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// Enable CRA’s built‑in offline support + installability
-serviceWorkerRegistration.register();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
-// Optional: performance measurement
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
